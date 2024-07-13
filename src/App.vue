b62bf90ae0d611c12014f12267c2a3f28ce95060
景行 <9024289+jayyang5213@user.noreply.gitee.com>
2024-07-13 20:43:14 +0800

    中英国际化处理

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { useUserStore } from '@/stores'
+import zh from 'element-plus/es/locale/lang/zh-cn.mjs'
 
 const userStore = useUserStore()
 </script>
 
 <template>
-  <router-view></router-view>
+  <!-- 国际化处理 -->
+  <el-config-provider :locale="zh">
+    <router-view />
+  </el-config-provider>
 </template>
 
 <style scoped></style>
