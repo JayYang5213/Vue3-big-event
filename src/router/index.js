3ea53fd01c61a1b7d1690382281da0db49010217
景行 <9024289+jayyang5213@user.noreply.gitee.com>
2024-07-13 16:15:32 +0800

    登录访问拦截

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,4 +35,12 @@ const router = createRouter({
   ],
 })
 
+// 登录访问拦截
+router.beforeEach((to) => {
+  if (!useUserStore().token && to.path !== '/login') {
+    return '/login'
+  }
+
+  return true
+})
 export default router
