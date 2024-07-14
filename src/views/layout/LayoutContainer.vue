e0651fe996a50de3d82361965a2f69e0cc9955a0
景行 <9024289+jayyang5213@user.noreply.gitee.com>
2024-07-14 18:19:03 +0800

    个人中心-基本资料页面功能实现

--- src/views/layout/LayoutContainer.vue
+++ src/views/layout/LayoutContainer.vue
@@ -108,7 +108,7 @@ const onCommand = async (command) => {
       <el-main>
         <router-view></router-view>
       </el-main>
-      <el-footer>大事件 ©2023 Created by 黑马程序员</el-footer>
+      <el-footer>大事件 ©2023 Created by xxx</el-footer>
     </el-container>
   </el-container>
 </template>
